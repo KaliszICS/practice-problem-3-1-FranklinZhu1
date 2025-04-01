770c32c61010cf6d817201fda65d90925cda9d17
franklin-zhu-ics4u1-02-25 <123124410+FranklinZhu1@users.noreply.github.com>
2025-04-01 14:04:44 +0000

    Create solution methods

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,20 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static int sum(String x, int total) {
+		if (x.equals("")) return total;
+		else return sum(x.substring(1), total + x.charAt(0) - 48);
 	}
 
-	
+	public static int sumOfDigits(int x) {
+		if (x < 0) x *= -1;
+		String num = Integer.toString(x);
+		return sum(num, 0);
+	}
+
+	public static int factorial(int n) {
+		if (n < 0) return 0;
+		if (n == 0) return 1;
+		return n*factorial(n - 1);
+	}
 
 }
